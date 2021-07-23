5859b0b581f6b4cb186f3d7cddc2d7cee4713588
kato hawkins <kristhahawkins@icloud.com>
2021-07-22 22:19:19 -0400

    created and updated Main.js

---
+++ mock-spotify/src/components/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Main = () =>{
+return(
+    <div className="main">
+   <div className="navHeader">dummy</div>
+   <div className="mainContent">
+     <h1>Uniquely Yours</h1>
+     <div className="cardsWrap">
+       <div className="card">
+         <div className="cardImage">
+<img src="https://images.unsplash.com/photo-1605114751089-698212d315e1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzl8fHBlYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="peace"/>
+
+
+
+         </div>
+         <div className="cardContent">
+         <h3>Liked Songs</h3> 
+         </div>
+       </div>
+     </div>
+   </div>
+  </div>
+)
+}
+export default Main;
